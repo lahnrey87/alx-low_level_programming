353cd9c3f9a271c57186db35c718057767b450e7
lahnrey87 <lahnreymos@gmail.com>
2022-12-13 06:33:21 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		_putchar(low);
+
+	_putchar('\n');
+
+	return (0);
+}
