6fe826003966fbd1b37237d0b7eca42ddf136734
lahnrey87 <lahnreymos@gmail.com>
2023-01-16 16:51:39 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,7 @@ struct dog
 	char *owner;
 };
 /**
- * dog_t typedef for struct dog
+ * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
